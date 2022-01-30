06f01e540a5a778fa13013c19140078193bdd397
Pulkit-s21 <pulkitsaxena795@gmail.com>
2022-01-30 18:08:39 +0530

    Reviews carousel in JS

---
+++ index.js
@@ -0,0 +1,126 @@
+// local reviews data
+const reviews = [
+    {
+      id: 1,
+      name: "susan smith",
+      job: "web developer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+      text:
+        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+      id: 2,
+      name: "anna johnson",
+      job: "web designer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+      text:
+        "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+    },
+    {
+      id: 3,
+      name: "peter jones",
+      job: "intern",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+      text:
+        "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    },
+    {
+      id: 4,
+      name: "bill anderson",
+      job: "the boss",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+      text:
+        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+    },
+    {
+      id: 5,
+      name: "samuel gunderson",
+      job: "intern",
+      img:
+        "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      text:
+        "The lift that sustains the kite in flight is generated when air moves around the kite's surface, producing low pressure above and high pressure below the wings. The interaction with the wind also generates horizontal drag along the direction of the wind. ",
+    },
+    {
+      id: 6,
+      name: "Jack Pearson",
+      job: "Fashion Blogger",
+      img:
+        "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      text:
+        "Fashion is defined in a number of different ways, and its application can be sometimes unclear. Though the term fashion connotes difference, as in the new fashions of the season.",
+    },
+    {
+      id: 7,
+      name: "Max Steel",
+      job: "Actor",
+      img:
+        "https://images.unsplash.com/photo-1629444291470-b9ad6e7a88e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHBvdHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+      text:
+        "Actors and actresses need to make a resume when applying for roles. The acting resume is very different from a normal resume; it is generally shorter, with lists instead of paragraphs, and it should have a head shot on the back.",
+    },
+    {
+      id: 8,
+      name: "Lucy Hart",
+      job: "Influencer",
+      img:
+        "https://images.unsplash.com/photo-1618151313441-bc79b11e5090?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      text:
+        "Influencer marketing (also known as influence marketing) is a form of social media marketing involving endorsements and product placement from influencers, people and organizations who have a purported expert level of knowledge or social influence in their field.",
+    },
+];
+
+// select items
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+// set starting item
+let currentItem = 0;
+
+// load initial item
+window.addEventListener("DOMContentLoaded", function () {
+    showPerson(currentItem);
+});
+
+// show next person
+nextBtn.addEventListener("click", function () {
+    currentItem++;
+    if (currentItem > reviews.length - 1) {
+        currentItem = 0;
+    }
+    showPerson(currentItem);
+});
+
+// show previous person
+prevBtn.addEventListener("click", function () {
+    currentItem--;
+    if (currentItem < 0) {
+        currentItem = reviews.length - 1;
+    }
+    showPerson(currentItem);
+});
+
+// show random person
+randomBtn.addEventListener("click", function () {
+    currentItem = Math.floor(Math.random() * reviews.length);
+    showPerson(currentItem);
+});
+
+// show person based on item 
+function showPerson(person) {
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+}
